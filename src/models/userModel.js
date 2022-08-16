dd5eab0cb4c4d69d5ce4c16c15be7924c49f283c
namanjha2002 <110611920+namanjha2002@users.noreply.github.com>
2022-08-17 00:44:41 +0530

    niyo

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -14,7 +14,7 @@ const bookSchema = new mongoose.Schema({
     category:{
         type:String,
         enum: ["Fantasy", "Dystopian", "Romance", "Science Fiction", "Historical", "Fiction"]
-    },
+    },          
     year: {
         type: Number,
         unique:true,
@@ -26,4 +26,4 @@ const bookSchema = new mongoose.Schema({
 
 ///////create a model from bookSchema with a name books in the database////////
 
-module.exports = mongoose.model('Book', bookSchema) //bookss
+module.exports = mongoose.model('Book', bookSchema) //books
