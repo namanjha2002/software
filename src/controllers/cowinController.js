f438b2d3e92dd29899a804f3069d00f73d063753
namanjha2002 <110611920+namanjha2002@users.noreply.github.com>
2022-09-02 21:18:42 +0530

    done

--- src/controllers/cowinController.js
+++ src/controllers/cowinController.js
@@ -1,85 +1,78 @@
 let axios = require("axios")
 
+// vaccine by district 
 
-let getStates = async function (req, res) {
-
-    try {
-        let options = {
-            method: 'get',
-            url: 'https://cdn-api.co-vin.in/api/v2/admin/location/states'
-        }
-        let result = await axios(options);
-        console.log(result)
-        let data = result.data
-        res.status(200).send({ msg: data, status: true })
-    }
-    catch (err) {
-        console.log(err)
-        res.status(500).send({ msg: err.message })
-    }
-}
-
-
-let getDistricts = async function (req, res) {
-    try {
-        let id = req.params.stateId
-        let options = {
-            method: "get",
-            url: `https://cdn-api.co-vin.in/api/v2/admin/location/districts/${id}`
-        }
-        let result = await axios(options);
-        console.log(result)
-        let data = result.data
-        res.status(200).send({ msg: data, status: true })
-    }
-    catch (err) {
-        console.log(err)
-        res.status(500).send({ msg: err.message })
-    }
-}
-
-let getByPin = async function (req, res) {
-    try {
-        let pin = req.query.pincode
+const getVacSession = async function(req,res){
+    try{
+        let id = req.query.id
         let date = req.query.date
-        console.log(`query params are: ${pin} ${date}`)
-        var options = {
-            method: "get",
-            url: `https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByPin?pincode=${pin}&date=${date}`
+        let option = {
+            method : "get",
+            url : `https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?district_id=${id}&date=${date}`
         }
-        let result = await axios(options)
-        console.log(result.data)
-        res.status(200).send({ msg: result.data })
+        let result = await axios(option)
+        res.status(200).send(result.data)
     }
-    catch (err) {
-        console.log(err)
-        res.status(500).send({ msg: err.message })
+    catch(err){
+        res.status(500).send(err.message)
     }
 }
 
-let getOtp = async function (req, res) {
-    try {
-        let blahhh = req.body
-        
-        console.log(`body is : ${blahhh} `)
-        var options = {
-            method: "post",
-            url: `https://cdn-api.co-vin.in/api/v2/auth/public/generateOTP`,
-            data: blahhh
-        }
+// city with temperature
 
-        let result = await axios(options)
-        console.log(result.data)
-        res.status(200).send({ msg: result.data })
+const getCity = async function(req,res){
+    try{
+        // let city = req.query.city
+        let key = req.query.key
+        let cityWithTemp = []
+        let city= ["Bengaluru","Mumbai", "Delhi", "Kolkata", "Chennai", "London", "Moscow"]
+        for(let i=0;i<city.length;i++){
+            let options = {
+                method:'get',
+                url : `http://api.openweathermap.org/data/2.5/weather?q=${city[i]}&appid=${key}`
+            }
+            let result = await axios(options)
+            cityWithTemp.push({city:city[i],temp:result.data.main['temp']})
+        }
+        let sortedCity = cityWithTemp.sort((a,b) => a.temp - b.temp)
+        res.status(200).send(sortedCity)
     }
-    catch (err) {
-        console.log(err)
-        res.status(500).send({ msg: err.message })
+    catch(err){
+        res.status(500).send(err.message)
     }
 }
 
+// meme creation
 
-module.exports.getStates = getStates
-module.exports.getDistricts = getDistricts
-module.exports.getByPin = getByPin
-module.exports.getOtp = getOtp
+const imageFlip = async function(req,res){
+    try{
+        let imageId = req.query.imageId
+        let textOne = req.query.textOne
+        let textTwo = req.query.textTwo
+        let userName = req.query.userName
+        let password = req.query.password
+        if(!textTwo){
+            option = {
+                method : 'post',
+                url : `https://api.imgflip.com/caption_image?template_id=${imageId}&text0=${textOne}&username=${userName}&password=${password}`
+            }   
+        }else
+        {
+            option = {
+                method : 'post',
+                url : `https://api.imgflip.com/caption_image?template_id=${imageId}&text0=${textOne}&text1=${textTwo}&username=${userName}&password=${password}`
+            }
+        }
+        let editedImg = await axios(option)
+        res.status(200).send(editedImg.data) 
+    }
+    catch(err){
+        res.status(500).send(err.message)
+    }
+
+}
+
+
+module.exports.getVacSession = getVacSession
+module.exports.getCity = getCity
+module.exports.imageFlip = imageFlip
