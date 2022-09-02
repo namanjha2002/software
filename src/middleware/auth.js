e9f9c237e427f398ca10e67336126970ac063918
namanjha2002 <110611920+namanjha2002@users.noreply.github.com>
2022-09-02 15:11:32 +0530

    doneee

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,12 +1,27 @@
-const authenticate = function(req, req, next) {
-    //check the token in request header
-    //validate this token
+const jwt = require("jsonwebtoken");
 
-    next()
+const verification = function(req, res, next){
+    let token = req.headers["x-auth-token"]
+    if(!token)
+    {
+        return res.send({ status: false, msg: "Token must be present" })
+    }
+    else
+    {
+        
+        let decodedToken = jwt.verify(token, "This is a secret information");
+        console.log(decodedToken)
+        let verUser = decodedToken.userId
+        let uId = req.params.userId
+        if(verUser == uId){
+
+            next()
+        }
+        else {
+          return res.send({ status: false, msg: "Token is invalid" });
+        }
+    }
+   
 }
 
-
-const authorise = function(req, res, next) {
-    // comapre the logged in user's id and the id in request
-    next()
-}
+module.exports.verification = verification
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,20 +1,20 @@
 const express = require('express');
 const router = express.Router();
 const userController= require("../controllers/userController")
+const middleWare=require("../middleware/auth")
 
 router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
 })
 
-router.post("/users", userController.createUser)
+router.post("/users", userController.createUser  )
 
 router.post("/login", userController.loginUser)
 
 //The userId is sent by front end
-router.get("/users/:userId", userController.getUserData)
-router.post("/users/:userId/posts", userController.postMessage)
+router.get("/users/:userId", auth.verification,userController.getUserData)
 
-router.put("/users/:userId", userController.updateUser)
-router.delete('/users/:userId', userController.deleteUser)
+router.put("/users/:userId", auth.verification,userController.updateUser)
+router.delete("/users/:userId",  auth.verification,userController.markUser) 
 
 module.exports = router;
