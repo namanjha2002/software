d0987c234241f2d738b1b106b0e3e863ce201244
rahulravidas024 <rahulravidas024@gmail.com>
2022-09-20 12:45:55 +0530

    create all folders

---
+++ src/routes/route.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const router = express.Router()
+const userController = require('../controllers/userController')
+const bookController = require('../controllers/bookController')
+const reviewController = require('../controllers/reviewController')
+const middleware = require('../middlewares/auth')
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.export = router
